4d5677d80f1d1c01681cb1ba2d9e8e843e4ba573
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-02-25 02:02:28 +0900

    fix display version and help description

---
+++ innodb-buffer-page-list/cmd.hpp
@@ -0,0 +1,5 @@
+#ifndef KT_INNODB_BUFFER_PAGE_LIST
+#define KT_INNODB_BUFFER_PAGE_LIST
+#define VERSION "0.0.1"
+#define CMD_CAPTION "Graphical display of InnoDB buffer pages"
+#endif
--- innodb-buffer-page-list/main.cpp
+++ innodb-buffer-page-list/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include "cmd.hpp"
 
 int main(int argc, char **argv) {
     using namespace boost::program_options;
-    options_description description("Options");
+    options_description description(CMD_CAPTION);
     description.add_options()
-            ("help,H", "command helps")
+            ("help,H", "show help")
             ("version,v", "version");
 
     variables_map variablesMap;
     store(parse_command_line(argc, argv, description), variablesMap);
     notify(variablesMap);
-    if (variablesMap.count("help"))
+    if (variablesMap.count("help")) {
         std::cout << description << std::endl;
+    } else if (variablesMap.count("version")) {
+        std::cout << VERSION << std::endl;
+    } else {
+        std::cout << description << std::endl;
+    }
 	return 0;
 }
 
