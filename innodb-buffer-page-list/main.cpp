700844187359bea6b21304de20006cd429c27345
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-02-27 09:43:33 +0900

    add database name option

--- innodb-buffer-page-list/main.cpp
+++ innodb-buffer-page-list/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv) {
             ("user,u", value<std::string>(),"mysql user name (required)")
             ("password,p", value<std::string>()->default_value(""),"mysql user password (optional)")
             ("host,h", value<std::string>()->default_value(DEFAULT_HOST), "mysql host (optional)")
-            ("port,P", value<unsigned int>()->default_value(DEFAULT_PORT),"mysql port number (optional)");
+            ("port,P", value<unsigned int>()->default_value(DEFAULT_PORT),"mysql port number (optional)")
+            ("database,D", value<std::string>()->default_value(""), "mysql database name (optional)");
 
     variables_map variablesMap;
     try {
@@ -36,6 +37,7 @@ int main(int argc, char **argv) {
         const unsigned int port = variablesMap["port"].as<unsigned int>();
         const std::string host = variablesMap["host"].as<std::string>();
         const std::string pass = variablesMap["password"].as<std::string>();
+        const std::string database = variablesMap["database"].as<std::string>();
     } catch (const boost::bad_any_cast& e) {
         std::cout << e.what() << std::endl;
         return 1;
