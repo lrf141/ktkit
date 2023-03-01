8f19da4ba55b7e2d019f3c05eceb73d2d7c33fcf
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-03-02 02:46:02 +0900

    change default port

--- innodb-buffer-page-list/cmd.hpp
+++ innodb-buffer-page-list/cmd.hpp
@@ -3,5 +3,6 @@
 #define VERSION "0.0.1"
 #define CMD_CAPTION "Graphical display of InnoDB buffer pages"
 #define DEFAULT_HOST "127.0.0.1"
-#define DEFAULT_PORT 3306
+// Use mysqlx protocol!!
+#define DEFAULT_PORT 33060
 #endif
