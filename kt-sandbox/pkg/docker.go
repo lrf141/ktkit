f1a1a4675762d73880cb31c8c3a700764d5c4a5d
Kento Takeuchi <lucky.rabbitfoot141@gmail.com>
2025-08-24 22:37:47 +0900

    add docker wrapper

---
+++ kt-sandbox/pkg/docker.go
@@ -0,0 +1,213 @@
+package pkg
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	DefaultImageRepository      = "mysql"
+	DefaultImageTag             = "8.4.6"
+	InternalNetworkName         = "kt-sandbox-internal"
+	InternalNetworkCidr         = "172.30.0.0/24"
+	InternalNetworkIpAddrFormat = "172.30.0.%d"
+)
+
+type ImageConfig struct {
+	repository string
+	tag        string
+}
+
+func NewImageConfig(repository string, tag string) *ImageConfig {
+	return &ImageConfig{
+		repository: repository,
+		tag:        tag,
+	}
+}
+
+func (c *ImageConfig) ImageUri() string {
+	return c.repository + ":" + c.tag
+}
+
+type DockerManager struct {
+	client      *client.Client
+	imageConfig *ImageConfig
+	network     *network.NetworkingConfig
+}
+
+func NewDockerManager(repository string, tag string) (*DockerManager, error) {
+	imageConfig := NewImageConfig(repository, tag)
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DockerManager{
+		client:      apiClient,
+		imageConfig: imageConfig,
+	}, nil
+}
+
+func (d *DockerManager) ExistImage(ctx context.Context) (bool, error) {
+	images, err := d.client.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		return false, err
+	}
+	for _, i := range images {
+		for _, imageTag := range i.RepoTags {
+			if imageTag == d.imageConfig.ImageUri() {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+func (d *DockerManager) PullImage(ctx context.Context) error {
+	existImage, err := d.ExistImage(ctx)
+	if err != nil {
+		return err
+	}
+	if existImage {
+		fmt.Printf("Already pulled image %s\n", d.imageConfig.ImageUri())
+		return nil
+	}
+
+	reader, err := d.client.ImagePull(ctx, d.imageConfig.ImageUri(), image.PullOptions{})
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	io.Copy(os.Stdout, reader)
+	return nil
+}
+
+func (d *DockerManager) CreateContainer(ctx context.Context, name string, index int, networkId string) (string, error) {
+	config := &container.Config{
+		Image: d.imageConfig.ImageUri(),
+		Tty:   true,
+		Env: []string{
+			"MYSQL_ROOT_PASSWORD=root",
+		},
+	}
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"3306/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: fmt.Sprintf("3306%d", index),
+				},
+			},
+		},
+	}
+	networkConfig := &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{
+			InternalNetworkName: {
+				NetworkID: networkId,
+				// the value of `index` starts at 0.
+				IPAddress: fmt.Sprintf(InternalNetworkIpAddrFormat, index+1),
+			},
+		},
+	}
+
+	c, err := d.client.ContainerCreate(ctx, config, hostConfig, networkConfig, nil, name)
+	if err != nil {
+		return "", err
+	}
+	return c.ID, nil
+}
+
+func (d *DockerManager) StartContainer(ctx context.Context, id string) error {
+	err := d.client.ContainerStart(ctx, id, container.StartOptions{})
+	if err != nil {
+		return err
+	}
+
+	maxWait := 10 * time.Second
+	interval := 1 * time.Second
+	deadline := time.Now().Add(maxWait)
+	for {
+		result, err := d.ExecAttachContainer(ctx, id, []string{"/bin/sh", "-c", "mysqladmin ping -uroot -proot"})
+		if err != nil {
+			return err
+		}
+
+		if result == "mysqld is alive" {
+			break
+		}
+
+		if time.Now().After(deadline) {
+			return fmt.Errorf("timed out waiting for container to start")
+		}
+		time.Sleep(interval)
+	}
+
+	return nil
+}
+
+func (d *DockerManager) CreateNetwork(ctx context.Context) (string, error) {
+	networkConfig := network.CreateOptions{
+		Driver: "bridge",
+		Options: map[string]string{
+			"subnet": InternalNetworkCidr,
+		},
+	}
+	n, err := d.client.NetworkCreate(ctx, InternalNetworkName, networkConfig)
+	if err != nil {
+		return "", err
+	}
+	return n.ID, nil
+}
+
+// ExecAttachContainer
+// []string{"/bin/sh", "-c", "mysqladmin", "ping", "-u", "root", "-p", "root"},
+func (d *DockerManager) ExecAttachContainer(ctx context.Context, id string, cmd []string) (string, error) {
+	resp, err := d.client.ContainerExecCreate(ctx, id, container.ExecOptions{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	attachResp, err := d.client.ContainerExecAttach(ctx, resp.ID, container.ExecAttachOptions{})
+	if err != nil {
+		return "", err
+	}
+	defer attachResp.Close()
+
+	var buf bytes.Buffer
+	// ignore stderr
+	_, err = stdcopy.StdCopy(&buf, io.Discard, attachResp.Reader)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(buf.String()), nil
+}
+
+func (d *DockerManager) StopContainer(ctx context.Context, id string) error {
+	return d.client.ContainerStop(ctx, id, container.StopOptions{})
+}
+
+func (d *DockerManager) RemoveContainer(ctx context.Context, id string) error {
+	return d.client.ContainerRemove(ctx, id, container.RemoveOptions{
+		Force: true,
+	})
+}
+
+func (d *DockerManager) RemoveNetwork(ctx context.Context, id string) error {
+	return d.client.NetworkRemove(ctx, id)
+}
---
+++ kt-sandbox/pkg/docker_test.go
@@ -0,0 +1,104 @@
+package pkg_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/lrf141/ktkit/kt-sandbox/pkg"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os"
+	"os/exec"
+)
+
+const (
+	testContainerTag = "8.4.6-test"
+)
+
+var _ = Describe("Docker suite", func() {
+
+	var sut *pkg.DockerManager
+
+	var ctx context.Context
+
+	var containerId string
+	var networkId string
+
+	BeforeSuite(func() {
+		ctx = context.Background()
+		err := exec.Command("which", "docker").Run()
+		if err != nil {
+			fmt.Printf("Failed to setup pkg test, need docker: %v", err)
+			os.Exit(1)
+		}
+	})
+
+	BeforeEach(func() {
+		manager, err := pkg.NewDockerManager(pkg.DefaultImageRepository, testContainerTag)
+		if err != nil {
+			fmt.Printf("Failed to setup pkg test: %v", err)
+			os.Exit(1)
+		}
+		sut = manager
+	})
+
+	It("ExistImage", func() {
+		// Setup
+		_ = exec.Command("docker", "pull", pkg.DefaultImageRepository+":"+testContainerTag).Run()
+
+		// Exercise
+		exist, err := sut.ExistImage(ctx)
+
+		// Verify
+		Expect(err).NotTo(HaveOccurred())
+		Expect(exist).To(BeTrue())
+	})
+
+	It("PullImage", func() {
+		// Exercise
+		err := sut.PullImage(ctx)
+
+		// Verify
+		Expect(err).NotTo(HaveOccurred())
+		exist, err := sut.ExistImage(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(exist).To(BeTrue())
+	})
+
+	It("CreateNetwork", func() {
+		// Exercise
+		id, err := sut.CreateNetwork(ctx)
+
+		// Verify
+		Expect(err).NotTo(HaveOccurred())
+		Expect(id).NotTo(BeNil())
+
+		networkId = id
+	})
+
+	It("CreateContainer", func() {
+		// Exercise
+		id, err := sut.CreateContainer(ctx, "test", 0, networkId)
+
+		// Verify
+		Expect(err).NotTo(HaveOccurred())
+		Expect(id).NotTo(BeEmpty())
+
+		containerId = id
+	})
+
+	It("StartContainer", func() {
+		// Exercise
+		err := sut.StartContainer(ctx, containerId)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("Cleanup docker resource", func() {
+		// Exercise
+		err := sut.StopContainer(ctx, containerId)
+		Expect(err).NotTo(HaveOccurred())
+		err = sut.RemoveContainer(ctx, containerId)
+		Expect(err).NotTo(HaveOccurred())
+		err = sut.RemoveNetwork(ctx, networkId)
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
